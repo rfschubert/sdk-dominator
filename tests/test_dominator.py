98f5cd3d0addf19211aa4d367bc1753c1b44934f
Raphael Schubert <rfswdp@gmail.com>
2019-03-25 19:12:29 -0300

    updated unit tests

--- tests/test_dominator.py
+++ tests/test_dominator.py
@@ -7,34 +7,47 @@ from dominator.exceptions import InvalidTaxIDException, InvalidCPFException, Inv
 
 class DominatorTestCase(TestCase):
     def setUp(self):
-        self.FAKE_CPFS = [
+        self.FAKE_CPFs = [
             "12345678910", "123.456.789.10"
         ]
 
-        self.FAKE_CNPJS = [
+        self.FAKE_CNPJs = [
             "12345678000110", "12.345.678/0001-10"
+        ]
+
+        self.TRUE_CPFs = [
+            "077.703.749-10", "07770374910"
+        ]
+
+        self.TRUE_CNPJs = [
+            "15280995000169", "15.280.995/0001-69"
         ]
 
     def test_validate_tax_id(self):
         # validate user CPF
         with self.assertRaises(InvalidCPFException):
-            Dominator().validate_tax_id(self.FAKE_CPFS[0])
+            Dominator().validate_tax_id(self.FAKE_CPFs[0])
 
         with self.assertRaises(InvalidTaxIDException):
-            Dominator().validate_tax_id(self.FAKE_CPFS[0])
+            Dominator().validate_tax_id(self.FAKE_CPFs[0])
 
         with self.assertRaises(InvalidCPFException):
-            Dominator().validate_tax_id(self.FAKE_CPFS[1])
+            Dominator().validate_tax_id(self.FAKE_CPFs[1])
 
         # validate user CNPJ
         with self.assertRaises(InvalidCNPJException):
-            Dominator().validate_tax_id(self.FAKE_CNPJS[0])
+            Dominator().validate_tax_id(self.FAKE_CNPJs[0])
 
         with self.assertRaises(InvalidTaxIDException):
-            Dominator().validate_tax_id(self.FAKE_CNPJS[0])
+            Dominator().validate_tax_id(self.FAKE_CNPJs[0])
 
         with self.assertRaises(InvalidCNPJException):
-            Dominator().validate_tax_id(self.FAKE_CNPJS[1])
+            Dominator().validate_tax_id(self.FAKE_CNPJs[1])
+
+        Dominator().validate_tax_id(self.TRUE_CPFs[0])
+        Dominator().validate_tax_id(self.TRUE_CPFs[1])
+        Dominator().validate_tax_id(self.TRUE_CNPJs[0])
+        Dominator().validate_tax_id(self.TRUE_CNPJs[1])
 
 # todo: validate against SERPRO
 # todo: lock user account
